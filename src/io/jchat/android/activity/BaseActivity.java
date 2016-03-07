2852350adabe5ff2df40eb5625e0829e689a2512
zllzll33 <949255878@qq.com>
2016-03-07 16:59:19 +0800

    new

--- src/io/jchat/android/activity/BaseActivity.java
+++ src/io/jchat/android/activity/BaseActivity.java
@@ -37,8 +37,7 @@ public class BaseActivity extends Activity {
     protected int mAvatarSize;
     protected int mWidth;
     protected int mHeight;
-  int ii=0;
-    int b=0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
